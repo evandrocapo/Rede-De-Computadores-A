ba3db354eff2eb10bb2a77cb63c9f7408ce7c51a
Evandro Capovilla <vandinhocapovilla@live.com>
2019-02-23 02:36:36 -0300

    Envio dos arquivos prontos servidores agostinho

---
+++ Cliente.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+/* Cliente UDP */
+void main(int argc, char *argv[])
+{
+	int s, server_address_size;
+	unsigned short port;
+	struct sockaddr_in server;
+	char buf_send[200], buf_rec[2001], msg[201];
+
+	/*
+	* O primeiro argumento (argv[1]) � o endere�o IP do servidor
+	* O segundo argumento (argv[2]) � a porta do servidor
+	*/
+	if(argc != 3)
+	{
+		printf("Use: %s enderecoIP porta\n",argv[0]);
+		exit(1);
+	}
+	port = htons(atoi(argv[2]));
+
+	/* Cria um socket UDP (dgram) */
+	if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0)
+	{
+		perror("socket()");
+		exit(1);
+	}
+
+	/* Define o endere�o IP e a porta do servidor */
+	server.sin_family      = AF_INET;            /* Tipo do endere�o         */
+	server.sin_port        = port;               /* Porta do servidor        */
+	server.sin_addr.s_addr = inet_addr(argv[1]); /* Endere�o IP do servidor  */
+
+
+	do
+	{
+		printf("> ");
+		fgets(msg, sizeof(msg), stdin);
+		strtok(msg, "\n");
+		memset(buf_send,0,strlen(buf_send));
+
+		
+
+		if(strcmp(msg, "exit") == 0){
+			printf("Fechando\n");
+			exit(1);
+		}
+
+		if(strcmp(msg, "exit") != 0){
+			memcpy(buf_send, msg, strlen(msg));
+			strcat(buf_send, "\0");
+			//int msgLen = strlen(buf_send)+1;
+
+			/* Envia a mensagem no buffer para o servidor */
+			if (sendto(s, buf_send, strlen(buf_send)+1, 0, (struct sockaddr *)&server, sizeof(server)) < 0)
+			{
+				perror("sendto()");
+				exit(2);
+			}
+
+			fflush(stdin);
+
+			//printf("%d       %d\n", strlen(msg),msgLen);
+			memset(buf_send,0,strlen(buf_send));
+
+			server_address_size = sizeof(server);
+
+
+			int coco = sizeof(buf_rec);
+			printf("size of buf_rec %ld\n", coco);
+
+			if(recvfrom(s, buf_rec, coco, 0, (struct sockaddr *) &server,&server_address_size) < 0)
+			{
+				perror("recvfrom()");
+				exit(1);
+			}
+
+
+			printf("%s",buf_rec);
+		}
+	}while(strcmp(msg, "exit") != 0);
+
+	/* Fecha o socket */
+	
+	close(s);
+}
---
+++ Servidor.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+/*
+* Servidor UDP
+*/
+void main(int argc, char *argv[])
+{
+	int sockint,s, namelen, client_address_size, teste;
+	long lSize;
+	struct sockaddr_in client, server;
+	char buf_rec[200], ret[2001];
+	FILE *p;
+	unsigned short port;
+
+	/*
+	* Cria um socket UDP (dgram).
+	*/
+do{
+	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
+		perror("socket()");
+		exit(1);
+	}
+
+	port = htons(atoi(argv[1]));
+
+	/*
+	* Define a qual endere�o IP e porta o servidor estar� ligado.
+	* Porta = 0 -> faz com que seja utilizada uma porta qualquer livre.
+	* IP = INADDDR_ANY -> faz com que o servidor se ligue em todos
+	* os endere�os IP
+	*/
+	server.sin_family      = AF_INET;   /* Tipo do endere�o             */
+	server.sin_port        = port;         /* Escolhe uma porta dispon�vel */
+	server.sin_addr.s_addr = INADDR_ANY;/* Endere�o IP do servidor      */
+
+	/*
+	* Liga o servidor � porta definida anteriormente.
+	*/
+	if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0)
+	{
+		perror("bind()");
+		exit(1);
+	}
+
+	/* Imprime qual porta foi utilizada. */
+	printf("> Porta utilizada: %d\n", ntohs(server.sin_port));
+
+	/*
+	* Recebe uma mensagem do cliente.
+	* O endere�o do cliente ser� armazenado em "client".
+	*/
+	client_address_size = sizeof(client);
+
+	memset(buf_rec, 0, sizeof(buf_rec));
+	
+	if(recvfrom(s, buf_rec, sizeof(buf_rec), 0, (struct sockaddr *) &client,&client_address_size) < 0) // echo matheus echo joao = joaoeus
+	{
+		perror("recvfrom()");
+		exit(1);
+	}
+
+	memset(ret, 0, sizeof(ret));
+
+	p = popen(buf_rec, "r");
+	fseek (p , 0 , SEEK_END); // percorre o tamanho do arquivo
+  	lSize = ftell (p); // seta em lSize o tamanho do arquivo
+
+	
+
+  	rewind (p); // volta o arquivo no começo
+	int guardar = fread (ret,1,lSize,p);
+	//fread(ret, sizeof(char), 10, p);
+
+	if(guardar > 2000){ 
+	guardar = 2000;
+	ret[2000] = '\n';
+	ret[2001] = '\0';
+	}
+
+	// for(int k =0; k < strlen(ret)+1;k++){
+	// 	printf("%c", ret[k]);
+	// }
+
+	if (sendto(s, ret, guardar+1, 0, (struct sockaddr *)&client, client_address_size) < 0)
+	{
+		perror("sendto()");
+		exit(2);
+	}
+
+	/*
+	* Imprime a mensagem recebida, o endere�o IP do cliente
+	* e a porta do cliente
+	*/
+	printf("> Recebido o comando %s...\n",buf_rec);
+	printf("Quantidade: %i\n", guardar);
+	/*
+	* Fecha o socket.
+	*/
+	fclose(p);
+	close(s);
+	
+}while(1);
+}
